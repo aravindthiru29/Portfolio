5d381d814db6e65318bd5d18ac8d5d5c2f2dff13
Aravind Thiru <aravthiru131@gmail.com>
2026-01-28 12:37:26 +0530

    first commit

---
+++ assests/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["AI & Data science Student", "Pre-final year Student", "Java Learner","seeking Internship Opportunities"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
