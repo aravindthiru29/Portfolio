efa7342cc59e8b77f3c42c7dc237b7011b34394b
Aravind Thiru <aravthiru131@gmail.com>
2026-06-10 11:21:00 +0530

    first commit

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var typed = new Typed(".text", {
-        strings: ["AI & Data science Student", "Pre-final year Student", "Java Learner","seeking Internship Opportunities"],
+        strings: ["AI & Data science Student", "Final year Student", "Java Learner","seeking Internship Opportunities"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
